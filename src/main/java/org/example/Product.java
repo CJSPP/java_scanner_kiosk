57e941551131cd9d99cffffce2785c9435a507bc
CJSPP <106299370+CJSPP@users.noreply.github.com>
2023-06-02 20:41:20 +0900

    Update Product.java

--- src/main/java/org/example/Product.java
+++ src/main/java/org/example/Product.java
@@ -3,8 +3,7 @@ package org.example;
 public class Product extends Menu {
 
 
-    /*price는 Menu 클래스에서 상속 받는 것이 아닌 Product에 있는 것을 사용해야하는데
-     그러기 위해서 static을 붙여서 공유변수로 사용해야 한다.(static이 아닌 곳에서 참조 될 수 없기 때문)*/
+
     private double price;
 
     /*burger의 이름, 가격, 설명을 파라미터로 갖는 생성자 Product가
